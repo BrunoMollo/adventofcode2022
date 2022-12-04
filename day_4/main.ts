629c2fcb6f45903992531c098641c0eb95c732c4
BrunoMollo <dia.mollo.bruno@gmail.com>
2022-12-04 14:05:12 -0300

    day 4 part 1

---
+++ day_4/main.ts
@@ -0,0 +1,42 @@
+import readText from '../readText.js';
+
+class Range{
+    constructor(public start:number, public end:number){}
+
+
+    contains(other:Range):boolean{
+        if(this.start<=other.start && other.end<=this.end)
+            return true
+        else 
+            return false
+    }
+
+
+}
+
+
+
+
+
+const text=readText('./day_4/input.txt')
+
+const lines=text.split("\n").map((l)=>l.trim()).filter(l=>l.length>0);
+
+const rangesPairs:Range[][]=[]
+for( let l of lines){
+    let range:Range[]= l.split(",")                            // ["58-95", "59-96"]
+                            .map(r=>r.split("-"))               // [ ["58","95"], ["59","96"] ]
+                            .map(r=>r.map(x=>parseInt(x)))      // [ [58,95], [59,96] ]
+                            .map(r=>new Range(r[0], r[1]))      // [{"start":58,"end":95},{"start":56,"end":96}]
+
+    rangesPairs.push(range)
+}
+
+
+const redundandts=rangesPairs.filter( r=> r[0].contains(r[1]) || r[1].contains(r[0]) )
+
+const result = redundandts.length
+
+console.log(result)
+
+
