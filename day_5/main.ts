aa7ec9109257d7e1beceaa35fb75adbf09e93176
BrunoMollo <dia.mollo.bruno@gmail.com>
2022-12-05 23:37:42 -0300

    day 5 part 1

---
+++ day_5/main.ts
@@ -0,0 +1,43 @@
+import { readLines, readText } from '../readText.js';
+
+
+const stacks:string[][]=[]
+let lines=readLines('/day_5/starting.txt')
+const number_of_stacks=9
+for(let i=1; i<=number_of_stacks*4; i+=4){
+    stacks.push(lines.map(l=>l[i]).filter(l=>l!=undefined && l!=' '))
+}
+for(let s of stacks){
+    s.reverse()
+}
+
+
+type Move={amount: number, from: number, to:number }
+const moves:Move[]=[]
+lines=readLines('./day_5/moves.txt')
+for(let l of lines){
+    let move:Move={amount:0, from:0, to:0}
+
+    move.amount=+l.split(' ')[1]
+    move.from=+l.split(' ')[3]-1
+    move.to=+l.split(' ')[5]-1
+
+    moves.push(move)
+}
+
+for(let m of moves){
+    for(let i=0; i<m.amount;i++){
+        let popped=stacks[m.from].pop()
+        if(popped)
+            stacks[m.to].push(popped)
+    }  
+}
+
+const result=stacks.map(c=>(c.length==0)?[' ']:c)
+                    .map(c=>c.pop())
+                    .join('')
+
+console.log(result)
+
+
+
